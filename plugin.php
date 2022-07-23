0254f0e8e39dd49c6ea3a9a6f76b9b9d2d4e563e
Christian Pietras <cpietras@149.105.205.92.host.secureserver.net>
2022-07-23 08:40:21 +0000

    first commit

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+$id                = 'hewo';
+$version           = '0.1';
+$ilias_min_version = '7.0';
+$ilias_max_version = '7.9999';
+$responsible       = 'hello world gmbh';
+$responsible_mail  = 'support@localhost.com';
+$supports_cli_setup = false;
+#
+
