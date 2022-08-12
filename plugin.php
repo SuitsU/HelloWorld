ee9ebd16ee0b6140bc8fd520a7e8a5948dafacda
chrisIlias1993 <christian.pietras@colin-kiegel.com>
2022-08-12 09:42:07 +0000

    first commit

--- plugin.php
+++ plugin.php
@@ -6,6 +6,6 @@ $ilias_min_version = '7.0';
 $ilias_max_version = '7.9999';
 $responsible       = 'hello world gmbh';
 $responsible_mail  = 'support@localhost.com';
-$supports_cli_setup = false;
+# $supports_cli_setup = false;
 #
 
