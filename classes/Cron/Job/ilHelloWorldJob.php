1f27a415495f0a16bc4e7dfae1646d92f8469560
chrisIlias1993 <christian.pietras@colin-kiegel.com>
2022-09-09 13:04:22 +0000

    logger, banner, composer lorem ipsum

--- classes/Cron/Job/ilHelloWorldJob.php
+++ classes/Cron/Job/ilHelloWorldJob.php
@@ -11,6 +11,16 @@ class ilHelloWorldJob extends ilCronJob
         return "HelloWorldJob";
     }
 
+    public function getTitle() : string
+    {
+        return 'Chris amazing '.ilHelloWorldPlugin::PLUGIN_NAME.' CronJob';
+    }
+
+    public function getDescription() : string
+    {
+        return ilHelloWorldPlugin::getInstance()->txt("cron_description");
+    }
+
     /**
      * @inheritDoc
      */
@@ -40,7 +50,7 @@ class ilHelloWorldJob extends ilCronJob
      */
     public function getDefaultScheduleValue() : int
     {
-        return 60;
+        return 2;
     }
 
     /**
@@ -48,16 +58,91 @@ class ilHelloWorldJob extends ilCronJob
      */
     public function run() : ilCronJobResult
     {
+        /**
+         * @var $ilDB ilDBInterface
+         */
+        global $ilDB;
+
         $result = new ilCronJobResult();
         $result->setStatus(ilCronJobResult::STATUS_OK);
         $result->setCode(200);
-        $result->setMessage('This is a test! Hello World.');
+        $result->setMessage('This is a test! Hello World. Path: '.__DIR__);
 
-        $logFile = fopen("../cron.log", "w") or die("Unable to open file!");
-        $txt = print_r($result, true);
-        fwrite($logFile, $txt);
-        fclose($logFile);
+        $set = $ilDB->query("SELECT count(`id`) as `entity_amount`, max(`id`) as `highest_id`, max(`create_date`) as `newest_date` FROM il_adn_notifications WHERE 1;");
+        $records = $ilDB->fetchAssoc($set);
+        $ids = $records['entity_amount'];
+        $highest_id = $records['highest_id'];
+        $newest_date = intval($records['newest_date']);
 
+        Utilities::log('newest date: '. date('Y-m-d',$newest_date));
+        Utilities::log('date: '. date('Y-m-d',strtotime('now')));
+        $date1 = new DateTime(date('Y-m-d 00:00:01',$newest_date));
+        $date2 = new DateTime(date('Y-m-d 00:00:01',strtotime('now')));
+        Utilities::log('compare date: '. ($date1 == $date2)?'same':'not same');
+
+        if(($date1 != $date2)) {
+
+            $lipsum = new joshtronic\LoremIpsum();
+
+            if ($ilDB->tableExists('il_adn_notifications')) {
+                $ilDB->manipulate("INSERT INTO il_adn_notifications ".
+                    "(
+                    `id`,
+                    `title`,
+                    `body`,
+                    `type`,
+                    `type_during_event`,
+                    `dismissable`,
+                    `permanent`,
+                    `allowed_users`,
+                    `parent_id`,
+                    `created_by`,
+                    `last_update_by`,
+                    `active`,
+                    `limited_to_role_ids`,
+                    `limit_to_roles`,
+                    `interruptive`,
+                    `link`,
+                    `link_type`,
+                    `link_target`,
+                    `event_start`,
+                    `event_end`,
+                    `display_start`,
+                    `display_end`,
+                    `create_date`,
+                    `last_update`
+                ) VALUES (".
+                    $ilDB->quote(($highest_id+1), "integer").",".
+                    $ilDB->quote('Title #'.Utilities::generateRandomString(5).' for day: '.$date1->format('d.m.Y'), "text").",".
+                    $ilDB->quote($lipsum->words(15) . '... <a href="#">[read more here]</a>', "text").",".
+                    $ilDB->quote(3, "integer").",". #`type`,
+                    # 3,0,1,"[0,13,6]",,6,,1,"[""2""]",1,0,"",0,_top,1654960131,1654960131,1654960131,1654960131,1654960131,1654960131
+                    $ilDB->quote(3, "integer").",". #`type_during_event`,
+                    $ilDB->quote(1, "integer").",".  #`dismissable`,
+                    $ilDB->quote(1, "integer").",". #`permanent`,
+                    $ilDB->quote("[0,13,6]", "text").",". #`allowed_users`,
+                    "NULL,". #`parent_id`,
+                    $ilDB->quote(6, "integer").",".  #`created_by`,
+                    "NULL,".  #`last_update_by`,
+                    $ilDB->quote(1, "integer").",".  #`active`,
+                    $ilDB->quote('["2"]', "text").",". #`limited_to_role_ids`,
+                    $ilDB->quote(1, "integer").",". #`limit_to_roles`,
+                    $ilDB->quote(0, "integer").",".  #`interruptive`,
+                    $ilDB->quote("", "text").",". #`link`,
+                    $ilDB->quote(0, "integer").",". #`link_type`,
+                    $ilDB->quote("_top", "text").",". #`link_target`,
+                    $ilDB->quote(strtotime('now'), "text").",". #`event_start`,
+                    $ilDB->quote(strtotime('now'), "text").",". #`event_end`,
+                    $ilDB->quote(strtotime('now'), "text").",". #`display_start`,
+                    $ilDB->quote(strtotime('now'), "text").",". #`display_end`,
+                    $ilDB->quote(strtotime('now'), "text").",". #`create_date`,
+                    $ilDB->quote(strtotime('now'), "text"). #`last_update`
+                    ")");
+            }
+
+
+        }
         return $result;
     }
+
 }
---
+++ classes/Util/class.Utilities.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+class Utilities
+{
+    # /var/www/dev7/ilias/Customizing/global/plugins/Services/Cron/CronHook/HelloWorld/classes/Util/info.log
+    static $plugin_path;
+
+    static function init() {
+        self::$plugin_path = substr(__DIR__, 0, (strpos(__DIR__,'/classes')));
+    }
+
+    public static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function log(String $message, String $level='info') {
+        self::init();
+        $log_path = self::$plugin_path.'/logs/';
+
+        // throw new Exception($log_path);
+
+        if (!file_exists($log_path)) mkdir($log_path,0775);
+
+        if(!file_exists("$log_path/$level.log")) {
+            touch("$log_path/$level.log");
+        }
+
+        $logFile = fopen("$log_path/$level.log", "a");
+        $date = date('Y-m-d H:i:s');
+        fwrite($logFile, PHP_EOL."$date: $message");
+        fclose($logFile);
+    }
+
+
+}
