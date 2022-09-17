42ce884b240dd4ac18164d83850ca100f833134b
chrisIlias1993 <christian.pietras@colin-kiegel.com>
2022-09-17 09:27:04 +0000

    test verion compare

--- classes/Util/class.HelloWorldUtilities.php
+++ classes/Util/class.HelloWorldUtilities.php
@@ -19,6 +19,12 @@ class HelloWorldUtilities
         return $randomString;
     }
 
+    /**
+     * @param        $message
+     * @param String $level
+     * @return void
+     * @throws HelloWorldException
+     */
     public static function log($message, String $level='info') {
         if (gettype($message) == 'array') {
             $message = json_encode($message, JSON_PRETTY_PRINT);
@@ -30,19 +36,72 @@ class HelloWorldUtilities
         self::init();
         $log_path = self::$plugin_path.'/logs/';
 
-        // throw new Exception($log_path);
-
-        if (!file_exists($log_path)) mkdir($log_path,0775);
+        if (!file_exists($log_path)) mkdir($log_path,0774);
 
         if(!file_exists("$log_path/$level.log")) {
             touch("$log_path/$level.log");
+            chmod("$log_path/$level.log",0774);
         }
 
-        $logFile = fopen("$log_path/$level.log", "a");
-        $date = date('Y-m-d H:i:s');
-        fwrite($logFile, PHP_EOL."$date: $message");
-        fclose($logFile);
+        try {
+            $logFile = fopen("$log_path/$level.log", "a");
+            $date = date('Y-m-d H:i:s');
+            fwrite($logFile, PHP_EOL."$date: $message");
+            fclose($logFile);
+        } catch (Exception $ex) {
+            $ex = new HelloWorldException($ex);
+            $ex->addAdditionalInfo("$date: Error: [{$ex->getMessage()}] in [Line: {$ex->getLine()}], log_path: $log_path, level: $level, message: $message");
+            throw ($ex);
+        }
+
     }
 
+
+}
+class HelloWorldException extends \Exception {
+
+    /**
+     * @var string  $additionalInfo
+     */
+    private $additionalInfo = "";
+
+    /**
+     * @var Exception $parent_exception
+     */
+    private $parent_exception = null;
+
+    /**
+     * @param Exception $ex
+     */
+    public function __construct(\Exception $ex)
+    {
+        $this->parent_exception = $ex;
+        parent::__construct($ex->getMessage(), $ex->getCode(), $ex->getPrevious());
+    }
+
+    /**
+     * @param String $additionalInfo
+     * @return void
+     */
+    public function addAdditionalInfo(String $additionalInfo) {
+        $this->message = $this->parent_exception->getMessage() . PHP_EOL
+            . "AdditionalInformation:" . PHP_EOL
+            . $additionalInfo;
+    }
+
+    /**
+     * @return String|null
+     */
+    public function getAdditionalInfo() : String {
+        return $this->additionalInfo;
+    }
+
+    /**
+     * @return Exception
+     */
+    public function getParentException() : \Exception
+    {
+        return $this->parent_exception?? new \Exception("Empty Exception", 100);
+    }
 
 }
